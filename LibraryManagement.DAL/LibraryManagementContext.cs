e8fedf5e7442000734bdfcfb374727ef5cedc5bd
Creeford Sanchez <creewrk@gmail.com>
2025-03-23 22:46:16 -0700

    started-DbContext

--- LibraryManagement.DAL/LibraryManagementContext.cs
+++ LibraryManagement.DAL/LibraryManagementContext.cs
@@ -1,4 +1,22 @@
-﻿namespace LibraryManagement.DAL {
-    public class LibraryManagementContext {
+﻿using LibraryManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.DAL {
+    public class LibraryManagementContext : IdentityDbContext<IdentityUser> {
+        public LibraryManagementContext(DbContextOptions<LibraryManagementContext> options) : base(options) { }
+
+        public DbSet<Book> Books { set; get; }
+        public DbSet<BookReview> BookReviews { set; get; }
+        public DbSet<Event> Events { set; get; }
+        public DbSet<EventReview> EventReviews { set; get; }
+        public DbSet<CheckOut> CheckOuts { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+        }
+
     }
 }
--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,10 +1,28 @@
-namespace LibraryManagement
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagement.DAL;
+using LibraryManagement.Models;
+using LibraryManagement.BLL;
+
+namespace LibraryManagement {
+    public class Program {
+        public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<LibraryManagementContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddTransient<BookRepository>();
+            builder.Services.AddTransient<BookReviewRepository>();
+            builder.Services.AddTransient<EventRepository>();
+            builder.Services.AddTransient<EventReviewRepository>();
+            builder.Services.AddTransient<CheckOutRepository>();
+
+            builder.Services.AddTransient<BookService>();
+            builder.Services.AddTransient<BookReviewService>();
+            builder.Services.AddTransient<EventService>();
+            builder.Services.AddTransient<EventReviewService>();
+            builder.Services.AddTransient<CheckOutService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -12,8 +30,7 @@ namespace LibraryManagement
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
+            if (!app.Environment.IsDevelopment()) {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -34,3 +51,5 @@ namespace LibraryManagement
         }
     }
 }
+
+
